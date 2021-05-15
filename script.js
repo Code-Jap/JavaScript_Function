98acd5d1c9f2fb1fda007207920402f5ba500777
Jasper <80272820+Code-Jap@users.noreply.github.com>
2021-05-15 23:25:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+
+
+const paintWalls = function (wallSide, color) {
+
+    console.log("The " +  wallSide + " has been painted " + color);
+
+
+};
+
+
+paintWalls ("west wall", "grey");
+paintWalls ("south-east wall", "purple")
+
+/*
+
+paintWalls ("green");
+paintWalls ("yellow");
+paintWalls ("north wall", "orange")
+paintWalls ("south-east wall", "grey")
+
+*/
+
+
+
+
+
+
+
+
+/*
+
+let names = function (name1, name2){
+
+        console.log(name1 + " loves " +  name2 )
+ 
+};
+
+names ("Jasper", "Stef")
+
+*/
